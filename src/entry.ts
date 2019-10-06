6ca9616906e5208f51160437fbb96f5c57a66355
Tim van Osch <timvosch@pollex.nl>
2019-10-06 14:12:33 +0200

    Re-enable coverage checks

--- src/entry.ts
+++ src/entry.ts
@@ -1,10 +1,1 @@
-import { createServer } from "http";
-
-const pid = process.pid;
-
-const server = createServer((req, res) => {
-  res.write(`Hello from ${pid}`);
-  return res.end();
-});
-
-server.listen(process.env.PORT || 3000);
+// Silence is golden
